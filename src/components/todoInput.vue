b228d3bc14ceb9f3b973b4466892fc269b5ff8b1
mrKing <MrKing>
2018-07-26 14:46:41 +0300

    router complated

---
+++ src/components/todoInput.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+    .todo-input
+        div.error {{validation.firstError('todo.name')}}
+        input(
+            type="text"
+            placeholder="Todo Name"
+            autofocus
+            v-model="todo.name"
+            :class="{'valid-error' : validation.hasError('todo.name')}"
+            @keydown.enter="addTodo"
+        ).input
+</template>
+
+<script>
+
+import {Validator} from 'simple-vue-validator';
+
+let uniqId = 0;
+
+export default {
+    mixins: [require('simple-vue-validator').mixin],
+    validators: {
+        "todo.name"(value){
+            // console.log(value);
+            return Validator.value(value).required("Поле не может быть пустым");
+        }
+    },
+    data(){
+        return{
+            todo: {
+                id: 0,
+                name: '',
+                checked: false
+            }
+        }
+    },
+    methods:{
+        addTodo(){
+            this.$validate().then(success => {
+                if (!success) return;
+
+                uniqId++
+                this.todo.id = uniqId;
+                this.$emit('addTodo', {...this.todo}); // три точки копируют объект это ЕS6
+                this.todo.name = "";
+                this.validation.reset();
+            });
+             
+        }
+    }
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+.input {
+    font-size: 24px;
+    padding: 16px 16px 16px 60px;
+    border: 1px solid transparent;
+    background: rgba(0, 0, 0, 0.003);
+    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+    line-height: 1.4em;
+    outline: none;
+    color: inherit;
+    width: 100%;
+    background: #fff;
+    
+}
+
+.todo-input{
+    position: relative;
+}
+
+.valid-error{
+    border: 1px solid red;
+}
+
+.error {
+  position: absolute;
+  top: -30px;
+  left: 0;
+  color: firebrick;
+}
+</style>
